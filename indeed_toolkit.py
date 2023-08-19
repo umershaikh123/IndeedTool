3747a2f3a1401546add377aa43bb90c1c248eb81
Muhammad umer <umershaikh217@gmail.com>
2023-08-19 18:52:45 +0500

    toolKit

--- indeed_toolkit.py
+++ indeed_toolkit.py
@@ -1,13 +1,18 @@
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
 
+# Import your custom tools
+from jobSearching import JobSearchingTool
+from jobApplying import JobApplyingTool
 
-class MyToolkit(BaseToolkit):
-name: str = "My Toolkit"
-description: str = "Description of my toolkit"
+class IndeedToolkit(BaseToolkit):
+    name: str = "Indeed Toolkit"
+    description: str = "Custom toolkit for job searching and applying on indeed.com"
 
-def get_tools(self) -> List[BaseTool]:
-return [MyTool()]
+    def get_tools(self) -> List[BaseTool]:
+        # Return instances of your custom tools
+        return [JobSearchingTool(), JobApplyingTool()]
 
-def get_env_keys(self) -> List[str]:
-return []
+    def get_env_keys(self) -> List[str]:
+        # If you need environment keys for your tools, specify them here
+        return ["YOUR_ENV_KEY_1", "YOUR_ENV_KEY_2"]
